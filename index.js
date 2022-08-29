93931631b41190a2a3587c669362e95b13e02e11
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-08-29 22:17:05 +0100

    Final Update

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const path = require("path");
-const {trackRecord} = require("./informationcollector-client/src/Utils/Logger")
+const {
+  trackRecord,
+} = require("./informationcollector-client/src/Utils/Logger");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 
-const buildPath = path.join(__dirname,  "./informationcollector-client/build",
-      );
+const buildPath = path.join(__dirname, "./informationcollector-client/build");
 app.use(express.static(buildPath));
 
-app.post("/app", async (req, res) => {  
- const {details} = req.body;
- await trackRecord(details);
- console.log("Interacting with frontend")
+app.post("/app", async (req, res) => {
+  const { details } = req.body;
+  await trackRecord(details);
+  console.log("Interacting with frontend");
   res.redirect("/");
 });
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(
     path.resolve(
       __dirname,
