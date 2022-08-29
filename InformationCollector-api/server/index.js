23c5ee27dcc1e3dbdbfdededa880a42741083e14
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-08-29 06:55:56 +0100

    Deploying app to Heroku

---
+++ InformationCollector-api/server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../../informationcollector-client/build')));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      "../../informationcollector-client/build",
+      "index.html"
+    )
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
